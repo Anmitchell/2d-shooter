913bbaa1ec6243b162f99d5a887fa016552fe0cf
Anmitchell <avarynmitchell@gmail.com>
2024-06-06 20:43:47 -0400

    added win condition and score ui

--- script.js
+++ script.js
@@ -152,14 +152,43 @@ class UI {
         this.game = game;
         this.fontSize = 25;
         this.fontFamily = "Helvetica";
-        this.color = 'yellow';
+        this.color = 'white';
     }
     draw(context) {
-        // ammo
+        context.save()
         context.fillStyle = this.color;
+        context.fillStyle = this.color;
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'black';
+
+        // score
+        context.fillText('Score: ' + this.game.score, 20 , 40);
+
+        // ammo
         for (let i = 0; i < this.game.ammo; i++) {
             context.fillRect(20 + 5 * i, 50, 3, 20);
         }
+
+        // game over messages
+        if (this.game.gameOver) {
+            context.textAlign = "center";
+            let message1;
+            let message2;
+            if (this.game.score > this.game.winningScore){
+                message1 = "You win!";
+                message2 = "Well done!";
+            }
+            else {
+                message1 = "You lose!";
+                message2 = "Try again next time!";
+            }
+            context.font = '50px' + this.fontFamily;
+            context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40);
+            context.font = '25px' + this.fontFamily;
+            context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5);
+        }
+        context.restore();
     }
 }
 
@@ -180,6 +209,8 @@ class Game {
         this.ammoTimer = 0;
         this.ammoInterval = 500;
         this.gameOver = false;
+        this.score = 0;
+        this.winningScore = 10;
     }
 
     update(deltaTime) {
@@ -205,6 +236,7 @@ class Game {
                     if (enemy.lives <= 0 ) {
                         enemy.markedForDeletion = true;
                         this.score += enemy.score; 
+                        if ( this.score > this.winningScore ) this.gameOver = true;
                     }
                 }
             })
